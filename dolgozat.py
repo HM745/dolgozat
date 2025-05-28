f56e080fbe10934926182a4b750b0d5416f414fe
HM745 <hmonpain09@gmail.com>
2025-05-28 12:09:58 +0200

    extra commit

--- dolgozat.py
+++ dolgozat.py
@@ -1,2 +1,34 @@
-print("1-es szék, 2-es szék, 3-as szék, 4-es szék ,5-ös szék")
-print("Melyiket szeretnéd lefoglalni?")
+print("1-es szék, 2-es szék, 3-as szék, 4-es szék ,5-ös szék(1,2,3,4,5)")
+szek_valasztas = int(input("Melyik széket szeretnéd lefoglalni?"))
+szekek = [1,2,3,4,5]
+if szek_valasztas == 1:
+    for i in range(1):
+        print(f"{szekek[0]}-es széket lefoglaltad")
+elif szek_valasztas == 2:
+    for i in range(1):
+        print(f"{szekek[1]}-es széket lefoglaltad")
+elif szek_valasztas == 3:
+    for i in range(1):
+        print(f"{szekek[2]}-es széket lefoglaltad")
+elif szek_valasztas == 4:
+    for i in range(1):
+        print(f"{szekek[3]}-es széket lefoglaltad")
+elif szek_valasztas == 3:
+    for i in range(1):
+        print(f"{szekek[len(szekek)-1]}-es széket lefoglaltad")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#extra commit
