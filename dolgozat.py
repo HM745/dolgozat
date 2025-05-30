4a5e084a8ee5a861cd962d5d8d769c1e125dab77
HM745 <hmonpain09@gmail.com>
2025-05-30 11:10:53 +0200

    8 szekes verzio

--- dolgozat.py
+++ dolgozat.py
@@ -1,6 +1,6 @@
 print("1-es szék, 2-es szék, 3-as szék, 4-es szék ,5-ös szék(1,2,3,4,5)")
 szek_valasztas = int(input("Melyik széket szeretnéd lefoglalni?"))
-szekek = [1,2,3,4,5]
+szekek = [1,2,3,4,5,6,7,8]
 if szek_valasztas == 1:
     for i in range(1):
         print(f"{szekek[0]}-es széket lefoglaltad")
@@ -15,7 +15,16 @@ elif szek_valasztas == 4:
         print(f"{szekek[3]}-es széket lefoglaltad")
 elif szek_valasztas == 5:
     for i in range(1):
-        print(f"{szekek[len(szekek)-1]}-ös széket lefoglaltad")
+        print(f"{szekek[4]}-ös széket lefoglaltad")
+elif szek_valasztas == 6:
+    for i in range(1):
+        print(f"{szekek[5]}-os széket lefoglaltad")
+elif szek_valasztas == 7: 
+    for i in range(1):
+        print(f"{szekek[6]}-es széket lefoglaltad")
+elif szek_valasztas == 8:
+    for i in range(1):
+        print(f"{szekek[len(szekek)-1]}-as széket lefoglaltad")
 else:
     print("Nem áll ilyen szék a rendelkezésre")
 
